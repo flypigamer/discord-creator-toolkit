4937b04188ab7ad40ba5b69b7cae1bc094b6af5f
flypigamer <degssfgsd@gmail.com>
2026-09-13 18:38:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const buttons = document.querySelectorAll(".tool-card button");
 
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const toolName = button.parentElement.querySelector("h3").textContent;
+
+    alert(toolName + " is coming soon! 🚀");
+  });
+});
